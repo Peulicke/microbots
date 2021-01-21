bb6b811596f240cdfaea9d2508758885dacb1613
Oscar <oscarrpeulicke@gmail.com>
2021-01-21 11:41:12 +0100

    Updates

--- static/js/main.174df777.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmicrobots=this.webpackJsonpmicrobots||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(1),s=c(0),r=c.n(s),o=c(3),i=c.n(o);c(9);var a=function(){return Object(n.jsx)("div",{className:"App",children:"test"})};i.a.render(Object(n.jsx)(r.a.StrictMode,{children:Object(n.jsx)(a,{})}),document.getElementById("root"))},9:function(t,e,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.174df777.chunk.js.map
