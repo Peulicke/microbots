a708cbd365e630cb8420177ce38f38718b0bf6e6
Oscar <oscarrpeulicke@gmail.com>
2021-03-01 14:59:48 +0100

    big cube

--- src/examples/bigCube.ts
+++ src/examples/bigCube.ts
@@ -3,7 +3,7 @@ export default {
     data: [
         [...Array(8)].map((_, i) => [...Array(8)].map((_, j) => [i - 3.5, 0.5, j - 3.5])).flat(),
         [...Array(4)]
-            .map((_, i) => [...Array(4)].map((_, j) => [...Array(4)].map((_, k) => [i - 3.5, k + 0.5, j - 3.5])))
+            .map((_, i) => [...Array(4)].map((_, j) => [...Array(4)].map((_, k) => [i - 1.5, k + 0.5, j - 1.5])))
             .flat()
             .flat()
     ]
