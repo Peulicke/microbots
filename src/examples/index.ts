877c1346f0079ba59e41b4768cdb920b8ef2507a
Oscar <oscarrpeulicke@gmail.com>
2021-03-01 14:47:16 +0100

    cube

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,8 +1,9 @@
 import { Vec3, Bot, World } from "../core";
 import towers from "./towers";
 import stack from "./stack";
+import cube from "./cube";
 
-export const examples = [towers, stack];
+export const examples = [towers, stack, cube];
 
 const toWorld = (index: number, time: number): World.World =>
     World.setBots(examples[index].data[time].map(p => Bot.setPos(Vec3.newVec3(p[0], p[1], p[2]))(Bot.newBot())))(
