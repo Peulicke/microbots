aad202d3028fd2e2d71ef09f42485cd4625557d5
Oscar <oscarrpeulicke@gmail.com>
2021-04-18 13:27:08 +0200

    cleaner code

--- src/core/Bot.test.ts
+++
@@ -1,8 +0,0 @@
-import { newBot } from "./Bot";
-
-it("creates a new bot", () => {
-    const bot = newBot({});
-    expect(bot.pos).toStrictEqual([0, 0, 0]);
-    expect(bot.target()).toStrictEqual(undefined);
-    expect(bot.weight).toStrictEqual(1);
-});
