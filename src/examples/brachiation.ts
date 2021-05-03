276be68d018b56ac4bb8ed4118844acad6ba5db0
Oscar <oscarrpeulicke@gmail.com>
2021-05-03 10:19:15 +0200

    lint

--- src/examples/brachiation.ts
+++ src/examples/brachiation.ts
@@ -4,7 +4,6 @@ import { createExample } from "./utils";
 const r = 10;
 const h1 = 7;
 const h2 = 12;
-const k = 6;
 
 const pillar = (pos: Vec3.Vec3, height: number): Vec3.Vec3[] =>
     [...Array(height)].map((_, i) => Vec3.add(pos, [0, i, 0]));
@@ -12,21 +11,6 @@ const pillar = (pos: Vec3.Vec3, height: number): Vec3.Vec3[] =>
 const bar = (pos: Vec3.Vec3, width: number): Vec3.Vec3[] => [...Array(width)].map((_, i) => Vec3.add(pos, [i, 0, 0]));
 
 const barZ = (pos: Vec3.Vec3, width: number): Vec3.Vec3[] => [...Array(width)].map((_, i) => Vec3.add(pos, [0, 0, i]));
-
-const p = (pos: Vec3.Vec3): Vec3.Vec3[] => {
-    const coords: Vec3.Vec3[] = [
-        [0, 0.5, 1],
-        [0, 0.5, 0],
-        [0, 0.5, -1],
-        [1, 0.5, 1],
-        [1, 0.5, 0],
-        [1, 0.5, -1],
-        [-1, 0.5, 1],
-        [-1, 0.5, 0],
-        [-1, 0.5, -1]
-    ];
-    return coords.map(v => Vec3.add(v, pos));
-};
 
 const platform = (pos: Vec3.Vec3, w: number, h: number): Vec3.Vec3[] => [
     ...pillar(Vec3.add(pos, [-w, 0, 0]), h),
