cbf24f6a2a35e597f564372cd448e605e581cecb
Oscar <oscarrpeulicke@gmail.com>
2021-06-12 23:36:53 +0200

    big arc

--- src/examples/bigArc.ts
+++ src/examples/bigArc.ts
@@ -1,7 +1,7 @@
 import { Vec3 } from "../core";
 import { createExample } from "./utils";
 
-const n = 20;
+const n = 40;
 
 const arc = [Vec3.newVec3(0, 0, 0)];
 
@@ -19,8 +19,8 @@ const m = Math.min(...arc.map(p => p[1]));
 arc.forEach((_, i) => (arc[i][1] = arc[i][1] - m + 0.5));
 
 const s = [
-    ...[...Array(n + 1)].map((_, i) => Vec3.newVec3(i + 15, 0.5, 0)),
-    ...[...Array(n)].map((_, i) => Vec3.newVec3(-i - 1 - 15, 0.5, 0))
+    ...[...Array(n + 1)].map((_, i) => Vec3.newVec3(Math.ceil(i / 2) + 4, 0.5, (i % 2) - 0.5)),
+    ...[...Array(n)].map((_, i) => Vec3.newVec3(-Math.floor(i / 2) - 1 - 4, 0.5, (i % 2) - 0.5))
 ];
 
 export default createExample("Big arc", [], s, arc);
